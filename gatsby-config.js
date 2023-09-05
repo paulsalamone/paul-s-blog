458d3e2226cb91be98cfb9752af17a6705c21efd
Paul Salamone <paul.salamone@db-n.com>
2023-09-05 17:38:04 +0200

    basic graphql query setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,24 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
+      },
+    },
+    `gatsby-plugin-image`,
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
 // styles?
 const mainStyles = {
@@ -9,6 +10,26 @@ const mainStyles = {
 };
 
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allContentfulBlogPost {
+        edges {
+          node {
+            id
+            title
+            content {
+              references {
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  console.log(data);
+
   return (
     <main style={mainStyles}>
       <h1>Paul's Blog</h1>
